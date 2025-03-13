35e254809bdae8a1c2edc5225a25984825a81ecf
nidqija <ajiqdin646@gmail.com>
2025-03-14 03:03:36 +0800

    added flask config

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import HomePage from './pages/homePage';
+import { useState , useEffect } from 'react';
 
 function App() {
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  } ,[])
+  
   return (
   <HomePage/>
   );
