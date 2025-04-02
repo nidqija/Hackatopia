3c6e88f64425f19800db437aa338cb01bdce15fb
nidqija <ajiqdin646@gmail.com>
2025-04-02 23:13:19 +0800

    added fix routing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,11 +10,11 @@ function Navbar1() {
             
           <Navbar.Brand href="#home" className='mx-3'>UNVIOLENCE!</Navbar.Brand>
           <Nav className='justify-content-end '>
-            <Nav.Link to="/"  className='mx-3 '>Home</Nav.Link>
-            <Nav.Link to="/initiatives" className='mx-3'>Intiatives</Nav.Link>
-            <Nav.Link to="/headquaters"  className='mx-3'>Our HQ</Nav.Link>
-            <Nav.Link to="/joinus"  className='mx-3'>Join Us</Nav.Link>
-            <Nav.Link to='/contactus' className='mx-3'>Contact Us</Nav.Link>
+            <Nav.Link path="/"  className='mx-3 '>Home</Nav.Link>
+            <Nav.Link path="/initiatives" className='mx-3'>Intiatives</Nav.Link>
+            <Nav.Link path="/headquaters"  className='mx-3'>Our HQ</Nav.Link>
+            <Nav.Link path="/joinus"  className='mx-3'>Join Us</Nav.Link>
+            <Nav.Link path='/contactus' className='mx-3'>Contact Us</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
