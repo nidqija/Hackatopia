ca8f61cd77d730d13b4b69a10e6b00136dfbd5d9
nidqija <ajiqdin646@gmail.com>
2025-04-02 23:21:27 +0800

    fixed routing 2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 function Navbar1() {
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark" className='p-3'>
         <Container>
-            
           <Navbar.Brand href="#home" className='mx-3'>UNVIOLENCE!</Navbar.Brand>
           <Nav className='justify-content-end '>
-            <Nav.Link path="/"  className='mx-3 '>Home</Nav.Link>
-            <Nav.Link path="/initiatives" className='mx-3'>Intiatives</Nav.Link>
-            <Nav.Link path="/headquaters"  className='mx-3'>Our HQ</Nav.Link>
-            <Nav.Link path="/joinus"  className='mx-3'>Join Us</Nav.Link>
-            <Nav.Link path='/contactus' className='mx-3'>Contact Us</Nav.Link>
+            <Nav.Link as={Link} to="/" className='mx-3'>Home</Nav.Link>
+            <Nav.Link as={Link} to="/initiatives" className='mx-3'>Initiatives</Nav.Link>
+            <Nav.Link as={Link} to="/headquaters" className='mx-3'>Our HQ</Nav.Link>
+            <Nav.Link as={Link} to="/joinus" className='mx-3'>Join Us</Nav.Link>
+            <Nav.Link as={Link} to="/contactus" className='mx-3'>Contact Us</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-    
-   
-
     </>
   );
 }
